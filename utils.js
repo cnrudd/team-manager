4ae51d8a76fc0f53ac4ad98244630ff0de7e8d48
Colin Rudd <cnrudd@gmail.com>
2019-10-15 06:29:46 -0400

    Better docs

--- utils.js
+++ utils.js
@@ -8,7 +8,8 @@ module.exports = {
      *
      * @param {number} max      highest possible number
      * @param {number} min     lowest possible number (defaults to 0)
-     * @return {number}         randomly generated number
+     * @return {number}         randomly generated number equal to or within
+     * the range specified by `min` & `max`.
      */
   randomNumber(max, min = 0) {
     return Math.floor(Math.random() * (max + 1 - min)) + min;
@@ -17,7 +18,8 @@ module.exports = {
   /**
    *
    * @param {Array} promiseArray The array of promises to run
-   * @return {Promise}
+   * @return {Promise} After all promises in `promiseArray` have been run
+   * sequentially, this returns a resolved promise.
    *
    * @see {@link https://css-tricks.com/why-using-reduce-to-sequentially-resolve-promises-works/}
    * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise}
